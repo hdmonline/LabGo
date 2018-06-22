201cb4581a45fa2235c640ef92ad3582d529c01c
Dongmin Han <dongminhan@gatech.edu>
2018-06-21 21:40:24 -0400

    (not working)

--- app/src/main/java/alpha/labgo/BuzzCardTextActivity.java
+++ app/src/main/java/alpha/labgo/BuzzCardTextActivity.java
@@ -3,72 +3,98 @@ package alpha.labgo;
 import android.content.Context;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.hardware.Camera;
+import android.hardware.camera2.CameraManager;
+import android.net.Uri;
+import android.os.Environment;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-
-import alpha.labgo.textrecognition.TextRecognitionProcessor;
 
 public class BuzzCardTextActivity extends AppCompatActivity {
 
     private static final int PERMISSION_REQUESTS = 2;
     private static final String TAG = "BuzzCardTextActivity";
 
-    private CameraSource cameraSource = null;
-    private CameraSourcePreview preview;
-    private GraphicOverlay graphicOverlay;
+    private int caller;
 
+    private Camera mCamera;
+    private CameraPreview  mCameraPreview;
+    private Camera.PictureCallback mPicture;
+    private Context mContext;
+    private ImageView mOverlay;
+    private SurfaceView mPreview;
+    private ImageButton mShutterButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_buzz_card_text);
 
-        preview = findViewById(R.id.camera_preview);
-        if (preview == null) {
-            Log.d(TAG, "Preview is null");
-        }
+        // check the caller
+        caller = getIntent().getIntExtra("caller", 0);
 
-        graphicOverlay = findViewById(R.id.camera_overlay);
-        if (graphicOverlay == null) {
-            Log.d(TAG, "graphicOverlay is null");
-        }
+        mPreview = findViewById(R.id.camera_preview);
 
         if (allPermissionsGranted()) {
-            createCameraSource();
+            createCameraPreview();
         } else {
             getRuntimePermissions();
         }
+
+        mShutterButton = findViewById(R.id.button_shutter);
+
+        mShutterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCamera.takePicture(null,null, mPictureCallBack);
+            }
+        });
     }
 
     @Override
     public void onResume() {
         super.onResume();
         Log.d(TAG, "onResume");
-        startCameraSource();
     }
 
     /** Stops the camera. */
     @Override
     protected void onPause() {
         super.onPause();
-        preview.stop();
+        releaseCameraAndPreview();
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-        if (cameraSource != null) {
-            cameraSource.release();
-        }
     }
 
+    private void createCameraPreview() {
+        // create an instance of rear camera
+        releaseCameraAndPreview();
+        int cameraId = findBackFacingCamera();
+        mCamera = getCameraInstance(cameraId);
+        mCameraPreview = new CameraPreview(getApplicationContext(), mCamera);
+        SurfaceView mPreview = findViewById(R.id.camera_preview);
+        SurfaceHolder surfaceHolder = mPreview.getHolder();
+
+    }
 
     private String[] getRequiredPermissions() {
         try {
@@ -124,45 +150,99 @@ public class BuzzCardTextActivity extends AppCompatActivity {
             int requestCode, String[] permissions, int[] grantResults) {
         Log.i(TAG, "Permission granted!");
         if (allPermissionsGranted()) {
-            createCameraSource();
+            createCameraPreview();
         }
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 
-    private void createCameraSource() {
-        // If there's no existing cameraSource, create one.
-        if (cameraSource == null) {
-            cameraSource = new CameraSource(this, graphicOverlay);
-        }
-
-        try {
-            Log.i(TAG, "Using Text Detector Processor");
-            cameraSource.setMachineLearningFrameProcessor(new TextRecognitionProcessor());
-        } catch (Exception e) {
-            Log.e(TAG, "can not create camera source");
-        }
-    }
-
-    /**
-     * Starts or restarts the camera source, if it exists. If the camera source doesn't exist yet
-     * (e.g., because onResume was called before the camera source was created), this will be called
-     * again when the camera source is created.
-     */
-    private void startCameraSource() {
-        if (cameraSource != null) {
-            try {
-                if (preview == null) {
-                    Log.d(TAG, "resume: Preview is null");
-                }
-                if (graphicOverlay == null) {
-                    Log.d(TAG, "resume: graphOverlay is null");
-                }
-                preview.start(cameraSource, graphicOverlay);
-            } catch (IOException e) {
-                Log.e(TAG, "Unable to start camera source.", e);
-                cameraSource.release();
-                cameraSource = null;
+    private int findBackFacingCamera() {
+        int cameraId = -1;
+        //Search for the back facing camera
+        //get the number of cameras
+        int numberOfCameras = Camera.getNumberOfCameras();
+        //for every camera check
+        for (int i = 0; i < numberOfCameras; i++) {
+            Camera.CameraInfo info = new Camera.CameraInfo();
+            Camera.getCameraInfo(i, info);
+            if (info.facing == Camera.CameraInfo.CAMERA_FACING_BACK) {
+                cameraId = i;
+                break;
             }
         }
+        return cameraId;
     }
+
+    private void releaseCameraAndPreview(){
+        if (mCamera != null){
+            mCamera.release();        // release the camera for other applications
+            mCamera = null;
+        }
+    }
+
+    /** A safe way to get an instance of the Camera object. */
+    public static Camera getCameraInstance(int id){
+        Camera c = null;
+        try {
+            c = Camera.open(id); // attempt to get a Camera instance
+        }
+        catch (Exception e){
+            // Camera is not available (in use or does not exist)
+        }
+        return c; // returns null if camera is unavailable
+    }
+
+    /** Create a File for saving an image or video */
+    private static File getOutputMediaFile() {
+        // To be safe, you should check that the SDCard is mounted
+        // using Environment.getExternalStorageState() before doing this.
+
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), "MyCameraApp");
+        // This location works best if you want the created images to be shared
+        // between applications and persist after your app has been uninstalled.
+
+        // Create the storage directory if it does not exist
+        if (!mediaStorageDir.exists()) {
+            if (!mediaStorageDir.mkdirs()) {
+                Log.d("MyCameraApp", "failed to create directory");
+                return null;
+            }
+        }
+        // Create a media file name
+        File mediaFile;
+
+        mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                "IMG_" + ".jpg");
+
+        return mediaFile;
+    }
+
+    /** Create a file Uri for saving an image or video */
+    private static Uri getOutputMediaFileUri(int type){
+        return Uri.fromFile(getOutputMediaFile());
+    }
+
+    // should put this in a handler class
+    private Camera.PictureCallback mPictureCallBack = new Camera.PictureCallback() {
+
+        @Override
+        public void onPictureTaken(byte[] data, Camera camera) {
+
+            File pictureFile = getOutputMediaFile();
+            if (pictureFile == null){
+                Log.d(TAG, "Error creating media file, check storage permissions: ");
+                return;
+            }
+
+            try {
+                FileOutputStream fos = new FileOutputStream(pictureFile);
+                fos.write(data);
+                fos.close();
+            } catch (FileNotFoundException e) {
+                Log.d(TAG, "File not found: " + e.getMessage());
+            } catch (IOException e) {
+                Log.d(TAG, "Error accessing file: " + e.getMessage());
+            }
+        }
+    };
 }
--- app/src/main/java/alpha/labgo/SignInActivity.java
+++ app/src/main/java/alpha/labgo/SignInActivity.java
@@ -174,11 +174,11 @@ public class SignInActivity extends BaseActivity implements View.OnClickListener
     public void onClick(View v) {
         int id = v.getId();
         if (id == R.id.button_camera) {
-            Intent toBuzzCard = new Intent(SignInActivity.this, BuzzCardBarcodeActivity.class);
+            Intent toBuzzCard = new Intent(SignInActivity.this, BuzzCardTextActivity.class);
             toBuzzCard.putExtra("caller", R.integer.FROM_CAMERA_BUTTON);
             startActivityForResult(toBuzzCard, GTID_REQUEST);
         } else if (id == R.id.button_sign_up) {
-            Intent toBuzzCard = new Intent(SignInActivity.this, BuzzCardBarcodeActivity.class);
+            Intent toBuzzCard = new Intent(SignInActivity.this, BuzzCardTextActivity.class);
             toBuzzCard.putExtra("caller", R.integer.FROM_SIGN_UP_BUTTON);
             startActivity(toBuzzCard);
         } else if (id == R.id.button_sign_in) {
